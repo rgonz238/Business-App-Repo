87210e67b529417805b80fb94179dae46503f6d2
Robert G <bobbybear2005@hotmail.com>
2021-03-21 17:51:15 -0700

    Removed binary directories, added tab control area for new orders form

--- BusinessApp/BusinessApp/frmNewOrder.Designer.cs
+++ BusinessApp/BusinessApp/frmNewOrder.Designer.cs
@@ -30,20 +30,22 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lblNewOrdCustName = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblOrderDate = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtBxInfo = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnSubmitOrder = new System.Windows.Forms.Button();
+            this.tabControlNewOrder = new System.Windows.Forms.TabControl();
+            this.tbPgOrderSummary = new System.Windows.Forms.TabPage();
+            this.tbPgRepair = new System.Windows.Forms.TabPage();
+            this.tbPgInstallUpgrade = new System.Windows.Forms.TabPage();
             this.label5 = new System.Windows.Forms.Label();
-            this.chkBxRepair = new System.Windows.Forms.CheckBox();
-            this.chkBxInstallUpgrade = new System.Windows.Forms.CheckBox();
-            this.chkBxNetwork = new System.Windows.Forms.CheckBox();
-            this.chkBxWeb = new System.Windows.Forms.CheckBox();
-            this.chkBxDatabase = new System.Windows.Forms.CheckBox();
-            this.chkBxPurchase = new System.Windows.Forms.CheckBox();
+            this.txtBxInfo = new System.Windows.Forms.TextBox();
+            this.tbPgNetworkService = new System.Windows.Forms.TabPage();
+            this.tbPgWebService = new System.Windows.Forms.TabPage();
+            this.tbPgDatabaseService = new System.Windows.Forms.TabPage();
+            this.tbPgPurchase = new System.Windows.Forms.TabPage();
+            this.tabControlNewOrder.SuspendLayout();
+            this.tbPgOrderSummary.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -66,16 +68,6 @@
             this.lblNewOrdCustName.TabIndex = 1;
             this.lblNewOrdCustName.Text = "<custName>";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Service Type:";
-            // 
             // lblOrderDate
             // 
             this.lblOrderDate.AutoSize = true;
@@ -96,150 +88,149 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Order Date:";
             // 
-            // txtBxInfo
-            // 
-            this.txtBxInfo.Location = new System.Drawing.Point(112, 169);
-            this.txtBxInfo.Multiline = true;
-            this.txtBxInfo.Name = "txtBxInfo";
-            this.txtBxInfo.Size = new System.Drawing.Size(476, 216);
-            this.txtBxInfo.TabIndex = 6;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(22, 169);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 17);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Order";
-            // 
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(395, 403);
+            this.btnCancel.Location = new System.Drawing.Point(341, 416);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(120, 30);
             this.btnCancel.TabIndex = 13;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // btnSubmit
+            // btnSubmitOrder
             // 
-            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(176, 404);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(120, 30);
-            this.btnSubmit.TabIndex = 12;
-            this.btnSubmit.Text = "Submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
-            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            this.btnSubmitOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitOrder.Location = new System.Drawing.Point(122, 417);
+            this.btnSubmitOrder.Name = "btnSubmitOrder";
+            this.btnSubmitOrder.Size = new System.Drawing.Size(120, 30);
+            this.btnSubmitOrder.TabIndex = 12;
+            this.btnSubmitOrder.Text = "Submit Order";
+            this.btnSubmitOrder.UseVisualStyleBackColor = true;
+            this.btnSubmitOrder.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tabControlNewOrder
+            // 
+            this.tabControlNewOrder.Controls.Add(this.tbPgOrderSummary);
+            this.tabControlNewOrder.Controls.Add(this.tbPgRepair);
+            this.tabControlNewOrder.Controls.Add(this.tbPgInstallUpgrade);
+            this.tabControlNewOrder.Controls.Add(this.tbPgNetworkService);
+            this.tabControlNewOrder.Controls.Add(this.tbPgWebService);
+            this.tabControlNewOrder.Controls.Add(this.tbPgDatabaseService);
+            this.tabControlNewOrder.Controls.Add(this.tbPgPurchase);
+            this.tabControlNewOrder.Location = new System.Drawing.Point(12, 70);
+            this.tabControlNewOrder.Name = "tabControlNewOrder";
+            this.tabControlNewOrder.SelectedIndex = 0;
+            this.tabControlNewOrder.Size = new System.Drawing.Size(588, 330);
+            this.tabControlNewOrder.TabIndex = 21;
+            // 
+            // tbPgOrderSummary
+            // 
+            this.tbPgOrderSummary.Controls.Add(this.label5);
+            this.tbPgOrderSummary.Controls.Add(this.txtBxInfo);
+            this.tbPgOrderSummary.Location = new System.Drawing.Point(4, 22);
+            this.tbPgOrderSummary.Name = "tbPgOrderSummary";
+            this.tbPgOrderSummary.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgOrderSummary.Size = new System.Drawing.Size(580, 304);
+            this.tbPgOrderSummary.TabIndex = 0;
+            this.tbPgOrderSummary.Text = "Order Summary";
+            this.tbPgOrderSummary.UseVisualStyleBackColor = true;
+            // 
+            // tbPgRepair
+            // 
+            this.tbPgRepair.Location = new System.Drawing.Point(4, 22);
+            this.tbPgRepair.Name = "tbPgRepair";
+            this.tbPgRepair.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgRepair.Size = new System.Drawing.Size(580, 408);
+            this.tbPgRepair.TabIndex = 1;
+            this.tbPgRepair.Text = "Repair";
+            this.tbPgRepair.UseVisualStyleBackColor = true;
+            // 
+            // tbPgInstallUpgrade
+            // 
+            this.tbPgInstallUpgrade.Location = new System.Drawing.Point(4, 22);
+            this.tbPgInstallUpgrade.Name = "tbPgInstallUpgrade";
+            this.tbPgInstallUpgrade.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgInstallUpgrade.Size = new System.Drawing.Size(580, 408);
+            this.tbPgInstallUpgrade.TabIndex = 2;
+            this.tbPgInstallUpgrade.Text = "Install/Upgrade";
+            this.tbPgInstallUpgrade.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(5, 186);
+            this.label5.Location = new System.Drawing.Point(1, 23);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 17);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "Information:";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Summary:";
             // 
-            // chkBxRepair
+            // txtBxInfo
             // 
-            this.chkBxRepair.AutoSize = true;
-            this.chkBxRepair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxRepair.Location = new System.Drawing.Point(136, 82);
-            this.chkBxRepair.Name = "chkBxRepair";
-            this.chkBxRepair.Size = new System.Drawing.Size(63, 17);
-            this.chkBxRepair.TabIndex = 15;
-            this.chkBxRepair.Text = "Repair";
-            this.chkBxRepair.UseVisualStyleBackColor = true;
-            this.chkBxRepair.CheckedChanged += new System.EventHandler(this.chkBxRepair_CheckedChanged);
+            this.txtBxInfo.Location = new System.Drawing.Point(86, 22);
+            this.txtBxInfo.Multiline = true;
+            this.txtBxInfo.Name = "txtBxInfo";
+            this.txtBxInfo.Size = new System.Drawing.Size(476, 264);
+            this.txtBxInfo.TabIndex = 18;
             // 
-            // chkBxInstallUpgrade
+            // tbPgNetworkService
             // 
-            this.chkBxInstallUpgrade.AutoSize = true;
-            this.chkBxInstallUpgrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxInstallUpgrade.Location = new System.Drawing.Point(290, 81);
-            this.chkBxInstallUpgrade.Name = "chkBxInstallUpgrade";
-            this.chkBxInstallUpgrade.Size = new System.Drawing.Size(114, 17);
-            this.chkBxInstallUpgrade.TabIndex = 16;
-            this.chkBxInstallUpgrade.Text = "Install/Upgrade";
-            this.chkBxInstallUpgrade.UseVisualStyleBackColor = true;
-            this.chkBxInstallUpgrade.CheckedChanged += new System.EventHandler(this.chkBxInstallUpgrade_CheckedChanged);
+            this.tbPgNetworkService.Location = new System.Drawing.Point(4, 22);
+            this.tbPgNetworkService.Name = "tbPgNetworkService";
+            this.tbPgNetworkService.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgNetworkService.Size = new System.Drawing.Size(580, 408);
+            this.tbPgNetworkService.TabIndex = 3;
+            this.tbPgNetworkService.Text = "Network Service";
+            this.tbPgNetworkService.UseVisualStyleBackColor = true;
             // 
-            // chkBxNetwork
+            // tbPgWebService
             // 
-            this.chkBxNetwork.AutoSize = true;
-            this.chkBxNetwork.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxNetwork.Location = new System.Drawing.Point(453, 82);
-            this.chkBxNetwork.Name = "chkBxNetwork";
-            this.chkBxNetwork.Size = new System.Drawing.Size(120, 17);
-            this.chkBxNetwork.TabIndex = 17;
-            this.chkBxNetwork.Text = "Network Service";
-            this.chkBxNetwork.UseVisualStyleBackColor = true;
-            this.chkBxNetwork.CheckedChanged += new System.EventHandler(this.chkBxNetwork_CheckedChanged);
+            this.tbPgWebService.Location = new System.Drawing.Point(4, 22);
+            this.tbPgWebService.Name = "tbPgWebService";
+            this.tbPgWebService.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgWebService.Size = new System.Drawing.Size(580, 408);
+            this.tbPgWebService.TabIndex = 4;
+            this.tbPgWebService.Text = "Web Service";
+            this.tbPgWebService.UseVisualStyleBackColor = true;
             // 
-            // chkBxWeb
+            // tbPgDatabaseService
             // 
-            this.chkBxWeb.AutoSize = true;
-            this.chkBxWeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxWeb.Location = new System.Drawing.Point(136, 119);
-            this.chkBxWeb.Name = "chkBxWeb";
-            this.chkBxWeb.Size = new System.Drawing.Size(99, 17);
-            this.chkBxWeb.TabIndex = 18;
-            this.chkBxWeb.Text = "Web Service";
-            this.chkBxWeb.UseVisualStyleBackColor = true;
-            this.chkBxWeb.CheckedChanged += new System.EventHandler(this.chkBxWeb_CheckedChanged);
+            this.tbPgDatabaseService.Location = new System.Drawing.Point(4, 22);
+            this.tbPgDatabaseService.Name = "tbPgDatabaseService";
+            this.tbPgDatabaseService.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgDatabaseService.Size = new System.Drawing.Size(580, 408);
+            this.tbPgDatabaseService.TabIndex = 5;
+            this.tbPgDatabaseService.Text = "Database Service";
+            this.tbPgDatabaseService.UseVisualStyleBackColor = true;
             // 
-            // chkBxDatabase
+            // tbPgPurchase
             // 
-            this.chkBxDatabase.AutoSize = true;
-            this.chkBxDatabase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxDatabase.Location = new System.Drawing.Point(290, 119);
-            this.chkBxDatabase.Name = "chkBxDatabase";
-            this.chkBxDatabase.Size = new System.Drawing.Size(127, 17);
-            this.chkBxDatabase.TabIndex = 19;
-            this.chkBxDatabase.Text = "Database Service";
-            this.chkBxDatabase.UseVisualStyleBackColor = true;
-            this.chkBxDatabase.CheckedChanged += new System.EventHandler(this.chkBxDatabase_CheckedChanged);
-            // 
-            // chkBxPurchase
-            // 
-            this.chkBxPurchase.AutoSize = true;
-            this.chkBxPurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkBxPurchase.Location = new System.Drawing.Point(453, 119);
-            this.chkBxPurchase.Name = "chkBxPurchase";
-            this.chkBxPurchase.Size = new System.Drawing.Size(79, 17);
-            this.chkBxPurchase.TabIndex = 20;
-            this.chkBxPurchase.Text = "Purchase";
-            this.chkBxPurchase.UseVisualStyleBackColor = true;
-            this.chkBxPurchase.CheckedChanged += new System.EventHandler(this.chkBxPurchase_CheckedChanged);
+            this.tbPgPurchase.Location = new System.Drawing.Point(4, 22);
+            this.tbPgPurchase.Name = "tbPgPurchase";
+            this.tbPgPurchase.Padding = new System.Windows.Forms.Padding(3);
+            this.tbPgPurchase.Size = new System.Drawing.Size(580, 408);
+            this.tbPgPurchase.TabIndex = 6;
+            this.tbPgPurchase.Text = "Purchase";
+            this.tbPgPurchase.UseVisualStyleBackColor = true;
             // 
             // frmNewOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(612, 458);
-            this.Controls.Add(this.chkBxPurchase);
-            this.Controls.Add(this.chkBxDatabase);
-            this.Controls.Add(this.chkBxWeb);
-            this.Controls.Add(this.chkBxNetwork);
-            this.Controls.Add(this.chkBxInstallUpgrade);
-            this.Controls.Add(this.chkBxRepair);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tabControlNewOrder);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtBxInfo);
+            this.Controls.Add(this.btnSubmitOrder);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lblOrderDate);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.lblNewOrdCustName);
             this.Controls.Add(this.label1);
             this.Name = "frmNewOrder";
             this.Text = "frmNewOrder";
+            this.tabControlNewOrder.ResumeLayout(false);
+            this.tbPgOrderSummary.ResumeLayout(false);
+            this.tbPgOrderSummary.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,19 +240,19 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblNewOrdCustName;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblOrderDate;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtBxInfo;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnSubmitOrder;
+        private System.Windows.Forms.TabControl tabControlNewOrder;
+        private System.Windows.Forms.TabPage tbPgOrderSummary;
+        private System.Windows.Forms.TabPage tbPgRepair;
+        private System.Windows.Forms.TabPage tbPgInstallUpgrade;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.CheckBox chkBxRepair;
-        private System.Windows.Forms.CheckBox chkBxInstallUpgrade;
-        private System.Windows.Forms.CheckBox chkBxNetwork;
-        private System.Windows.Forms.CheckBox chkBxWeb;
-        private System.Windows.Forms.CheckBox chkBxDatabase;
-        private System.Windows.Forms.CheckBox chkBxPurchase;
+        private System.Windows.Forms.TextBox txtBxInfo;
+        private System.Windows.Forms.TabPage tbPgNetworkService;
+        private System.Windows.Forms.TabPage tbPgWebService;
+        private System.Windows.Forms.TabPage tbPgDatabaseService;
+        private System.Windows.Forms.TabPage tbPgPurchase;
     }
 }
